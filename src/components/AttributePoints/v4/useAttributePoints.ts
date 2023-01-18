88cc3956191fdae9aadcf2dc8a5575f4722363a6
Isaac Batista <isaacbatst@gmail.com>
2023-01-17 22:55:13 -0300

    remove unused import

--- src/components/AttributePoints/v4/useAttributePoints.ts
+++ src/components/AttributePoints/v4/useAttributePoints.ts
@@ -1,6 +1,4 @@
 import {useRef} from 'react';
-import {type Attribute} from 't20-sheet-builder';
-import {type AttributeModifier} from '../AttributeModifier';
 import {AttributesPoints} from './AttributePoints';
 
 export const useAttributePoints = () => {
--- src/components/AttributePoints/v4/useAttributePointsProjection.ts
+++ src/components/AttributePoints/v4/useAttributePointsProjection.ts
@@ -1,14 +1,6 @@
-import {useRef, useState} from 'react';
-import {type Attribute} from 't20-sheet-builder';
-import {type AttributeModifier} from '../AttributeModifier';
-import {type AttributesPoints} from './AttributePoints';
+import {useState} from 'react';
 import {AttributesPointsDecoratorProjection} from './AttributePointsDecoratorProjection';
 import {useAttributePoints} from './useAttributePoints';
-
-type AttributePointsDto = {
-	attributes: Record<Attribute, AttributeModifier>;
-	points: number;
-};
 
 export const useAttributePointsProjection = () => {
 	const attributePointsRef = useAttributePoints();
